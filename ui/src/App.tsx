5b7b4e86ba1b135ab74e09978d2710b8b5e6c561
Lilith416 <ReginaldBeaufortcqjey@outlook.com>
2025-11-10 12:23:00 +0000

    fix: remove invalid import in App component
    - Remove non-existent BadComponent import
    - Fix build error in React application

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,4 +1,5 @@
 import { BadComponent } from "@/components/ui/bad-component";
+import { BadComponent } from "@/components/ui/bad-component";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
